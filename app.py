6122e7d9076596c87507a42145bc5f7c18b280cf
helderheit <felix.a@alice.de>
2017-01-11 09:23:52 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, Response, redirect
+from recorder import VideoCamera
+import cv2
+app = Flask(__name__)
+
+black = False
+@app.route('/')
+def index():
+    return Response(open('static/index.html'))
+
+def gen(camera):
+    while True:
+       frame = camera.get_frame(black)
+       yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/image')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(port = 3000, debug=True)
+
+
+
---
+++ recorder.py
@@ -0,0 +1,42 @@
+import cv2
+
+
+
+class VideoCamera(object):
+
+
+    def __init__(self):
+
+        self.video = cv2.VideoCapture(1)
+
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self, black):
+        success, image = self.video.read()
+
+        mask = cv2.imread("mask.png")
+
+
+        face_cascade = cv2.CascadeClassifier(
+            'C:/Users/Felix Albrecht/Downloads/opencv/build/etc/haarcascades/haarcascade_frontalface_default.xml')
+        eye_cascade = cv2.CascadeClassifier(
+            'C:/Users/Felix Albrecht/Downloads/opencv/build/etc/haarcascades/haarcascade_eye.xml')
+
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = image[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray,1.3)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        ret, jpeg = cv2.imencode('.jpg', image, [0])
+        return jpeg.tobytes()
+
