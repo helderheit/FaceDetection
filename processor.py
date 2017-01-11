4cbd87b6c8bb32da163930f28fe9d0bb77b1f03a
helderheit <felix.a@alice.de>
2017-01-11 13:24:51 +0100

    Add files via upload

--- processor.py
+++ processor.py
@@ -1,1 +1,36 @@
 #Bildverarbeitung
+import cv2
+
+
+
+def process_image(image):
+
+    #Face Detection
+
+    face_cascade = cv2.CascadeClassifier(
+        'haarcascades/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(
+        'haarcascades/haarcascade_eye.xml')
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = image[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    return image
+
+
+
+
+
+
+
+
+
--- recorder.py
+++ recorder.py
@@ -1,5 +1,6 @@
 import cv2
 
+import processor
 
 
 class VideoCamera(object):
@@ -7,7 +8,7 @@ class VideoCamera(object):
 
     def __init__(self):
 
-        self.video = cv2.VideoCapture(1)
+        self.video = cv2.VideoCapture(0)
 
 
     def __del__(self):
@@ -16,26 +17,10 @@ class VideoCamera(object):
     def get_frame(self, black):
         success, image = self.video.read()
 
-        mask = cv2.imread("mask.png")
-
-
-        face_cascade = cv2.CascadeClassifier(
-            'haarcascades/haarcascade_frontalface_default.xml')
-        eye_cascade = cv2.CascadeClassifier(
-            'haarcascades/haarcascade_eye.xml')
-
-
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = image[y:y + h, x:x + w]
-            eyes = eye_cascade.detectMultiScale(roi_gray,1.3)
-            for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+        image = processor.process_image(image)
 
         ret, jpeg = cv2.imencode('.jpg', image, [0])
         return jpeg.tobytes()
+
+
+
