38e30c6eadbe92f0cc4fdde4ff27a3ccedccc200
doGregor <gregor.donabauer@gmx.de>
2017-01-17 11:37:05 +0100

    Filter hinzugefügt
    Filter für Graubild und Negativbild

--- filters/filters.py
+++ filters/filters.py
@@ -1,1 +1,8 @@
 #Module fuer Filter
+import cv2
+
+def filter_gray(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def filter_negative(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
