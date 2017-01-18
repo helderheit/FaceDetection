5e5116ee9863e0d1c2eda7ea0c16af00b74671e8
doGregor <gregor.donabauer@gmx.de>
2017-01-18 14:03:13 +0100

    Germany Filter

--- filters/filters.py
+++ filters/filters.py
@@ -21,3 +21,14 @@ def filter_split(image):
 	blank_image[0:height_2, width_2:width] = small
 	blank_image[height_2:height, width_2:width] = small
 	return blank_image
+
+def filter_germany(image):
+	height, width = image.shape[:2]
+	height_3 = height/3
+	height_2 = height_3*2
+	blank_image = np.zeros((height, width, 3), np.uint8)
+	cv2.rectangle(blank_image, (0, 0), (width, height_3), (0, 0, 0), -1, 8, 0)
+	cv2.rectangle(blank_image, (0, height_3), (width, height_2), (0, 0, 255), -1, 8, 0)
+	cv2.rectangle(blank_image, (0, height_2), (width, height), (0, 255, 255), -1, 8, 0)
+	result = cv2.addWeighted(image, 0.5, blank_image, 0.5, 0)
+	return result
