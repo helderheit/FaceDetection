7d3d5336001389979b9b50bff2a001a4a09eee63
doGregor <gregor.donabauer@gmx.de>
2017-01-18 12:58:34 +0100

    Split Filter

--- filters/filters.py
+++ filters/filters.py
@@ -1,5 +1,6 @@
 #Module fuer Filter
 import cv2
+import numpy as np
 
 def filter_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -7,12 +8,16 @@ def filter_gray(image):
 def filter_negative(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-#http://stackoverflow.com/questions/4195453/how-to-resize-an-image-with-opencv2-0-and-python2-6
-#http://stackoverflow.com/questions/19098104/python-opencv2-cv2-wrapper-get-image-size
 def filter_split(image):
 	height, width = image.shape[:2]
 	height_2 = height/2
 	width_2 = width/2
-	small = cv2.resize(image, (height_2, width_2))
-	image[0:height_2, 0:width_2] = small
-	image[0:height_2, width_2:width] = small
+	#blank_image: "http://stackoverflow.com/questions/12881926/create-a-new-rgb-opencv-image-using-python"
+	blank_image = np.zeros((height, width, 3), np.uint8)
+	#resize: "http://docs.opencv.org/trunk/da/d6e/tutorial_py_geometric_transformations.html"
+	small = cv2.resize(image, (width_2, height_2), interpolation = cv2.INTER_CUBIC)
+	blank_image[0:height_2, 0:width_2] = small
+	blank_image[height_2:height, 0:width_2] = small
+	blank_image[0:height_2, width_2:width] = small
+	blank_image[height_2:height, width_2:width] = small
+	return blank_image
