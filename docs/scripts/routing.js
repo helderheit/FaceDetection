75840258dd744abd6689700f8bbdd13e730e6fc1
helderheit <felix.a@alice.de>
2017-02-06 15:58:38 +0100

    Bugfix

--- docs/scripts/routing.js
+++ docs/scripts/routing.js
@@ -1,7 +1,7 @@
 var url = "http://localhost:3000";
 var streamer = false;
 
-if(!window.location.href.includes('localhost')){
+if(window.location.href.includes('localhost')){
   console.log(window.location.href);
   streamer = true;
 }
